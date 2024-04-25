0427d023e2d1df24053bddecea43679a9bb5bffa
sourav0299 <105085605+sourav0299@users.noreply.github.com>
2024-04-25 17:01:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import yfinance as yf
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # List of ticker symbols for the portfolio
 portfolio_symbols = ['GAIL.NS', 'IDFCFIRSTB.NS', 'IOC.NS', 'IRFC.NS', 'JIOFIN.NS', 'NBCC.NS', 'NCC.NS', 'ONGC.NS', 'SAIL.NS', 'TATAPOWER.NS', 'TATASTEEL.NS', 'TNPETRO.NS', 'ZOMATO.NS']
 
-@app.route('/api/ltp/portfolio')
+@app.route('/api/ltp/portfolio', methods=['GET'])
 def get_portfolio_ltp():
     try:
         portfolio_ltp = {}
@@ -17,12 +19,15 @@ def get_portfolio_ltp():
             # Get the last price
             last_price = data.history(period="1d")["Close"].iloc[-1]
 
+            # Round the last price to two decimal places
+            last_price = round(last_price, 2)
+
             # Add the symbol and last price to the portfolio dictionary
             portfolio_ltp[symbol] = last_price
 
-        return jsonify(portfolio_ltp)
+        return jsonify(portfolio_ltp), 200
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
